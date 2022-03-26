0314534a62590a730d2fc9d1f42baaf197fe1e97
Corbeno <corbin@kemsfamily.us>
2022-03-26 16:44:01 -0500

    frontend work

---
+++ src/Search.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import "./Search.css";
+const Search = () => {
+    const [searchText, setSearchText] = useState("");
+    const [gameData, setGameData] = useState("empty");
+
+    React.useEffect(() => {
+        console.log('MyComponent onMount');
+        getData(setGameData);
+        return () => {
+            console.log('MyComponent onUnmount');
+        };
+    }, []);
+
+    if(gameData === "empty"){
+        //TODO return error?
+        return null;
+    }
+
+    return (
+        <div>
+            <h1>Idleon Insight</h1>
+            <div style={{display: "flex", alignItems: 'center'}}>
+                <input 
+                    name="text" 
+                    type="text" 
+                    placeholder="Search" 
+                    onChange={(event) => {
+                        let text = event.target.value;
+                        if(!isRegex(text)){
+                            //TODO handle inproper regex
+                            return;
+                        }
+                        setSearchText(text)
+                    }}
+                />
+                <p class="hovertext" data-hover="Accepts regular search or Regex. Ignores case">?</p>
+            </div>
+            {/* <button>Search</button> */}
+            <SearchResult searchText = {searchText} data={gameData}/>
+            {/* <p>{JSON.stringify(gameData)}</p> */}
+        </div>
+    );
+}
+
+const SearchResult = ({ searchText, data }) => {
+    let regex = null;
+    let displayData = null;
+    let regexParams = "i"; //i = ignore case
+    if(!isValidRegex(searchText)){
+        regex = new RegExp(regexEscape(searchText), regexParams);
+    }else{
+        regex = new RegExp(searchText, regexParams);
+    }
+    displayData = searchData(regex, data);
+    console.log(regex);
+    return (
+        <table>
+            <tr id="tr-header">
+                <th>Type</th>
+                <th>Name</th>
+                <th>Bonus</th>
+            </tr>
+            {displayData.map(function(each){
+                return(
+                    <tr>
+                        <th>{each.source || ""}</th>
+                        <th>{each.name}</th>
+                        <th>{each.bonuses}</th>
+                    </tr>
+                )
+            })}
+        </table>
+    );
+}
+
+function searchData(search, data){
+    let r = [];
+    for(let obj of data){
+        for(let bonus of obj.bonuses){
+            if(search.test(bonus)){
+                r.push(obj);
+                continue;
+            }
+        }
+    }
+    return r;
+}
+
+function isValidRegex(str){
+    try{
+        new RegExp(str);
+    }catch(e){
+        return false;
+    }
+    return true;
+}
+
+function regexEscape(str) {
+    return str.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
+}
+
+//Get Method
+function getData(setJson){
+    fetch("https://raw.githubusercontent.com/Corbeno/Idleon-Insight/main/output/output.json")
+        .then((response) => response.json())
+        .then((json) => {
+            setJson(json);
+    });
+};
+
+
+function isRegex(text){
+    return true;
+}
+export default Search;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Search from './Search';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Search />
   </React.StrictMode>,
   document.getElementById('root')
 );
