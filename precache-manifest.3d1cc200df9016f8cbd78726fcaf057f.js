29e26295bc65b38a476b4693e6f812ce5df69ad3
Corbeno <corbin@kemsfamily.us>
2022-03-26 16:43:07 -0500

    Updates

---
+++ precache-manifest.3d1cc200df9016f8cbd78726fcaf057f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf5a935395eec3b9c715",
+    "url": "/Idleon-Insight/static/css/main.1eafc09c.chunk.css"
+  },
+  {
+    "revision": "cf5a935395eec3b9c715",
+    "url": "/Idleon-Insight/static/js/main.cf5a9353.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/Idleon-Insight/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/Idleon-Insight/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "771c2920fca3fc21c3d8",
+    "url": "/Idleon-Insight/static/js/runtime~main.771c2920.js"
+  },
+  {
+    "revision": "881eb136d188ab7fd509b3d500db101f",
+    "url": "/Idleon-Insight/index.html"
+  }
+];
